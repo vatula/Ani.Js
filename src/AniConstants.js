9e407fcf2f42bb3188c12adccc128d0c49dfde12
Bogdan Vatulya <bogdan.watula@gmail.com>
2011-11-09 10:42:17 +0000

    quick fix for AniConstants.js

--- src/AniConstants.js
+++ src/AniConstants.js
@@ -1,34 +1,34 @@
 var Ani = Ani || {};
 
 Ani.Constants = Ani.Constants || {
-    VERSION = "2.0",
+    VERSION: "2.0",
 
     // timeMode
-    SECONDS = "SECONDS",
-    FRAMES = "FRAMES",
+    SECONDS: "SECONDS",
+    FRAMES: "FRAMES",
 
     // callback, keywords for the property list string parsing
-    ON_START = "onStart",
-    ON_END = "onEnd",
+    ON_START: "onStart",
+    ON_END: "onEnd",
 
     // playMode
-    FORWARD = "FORWARD",
-    BACKWARD = "BACKWARD",
-    YOYO = "YOYO",
+    FORWARD: "FORWARD",
+    BACKWARD: "BACKWARD",
+    YOYO: "YOYO",
 
     // autoStartMode
-    AUTOSTART = "AUTOSTART",
-    NO_AUTOSTART = "NO_AUTOSTART",
+    AUTOSTART: "AUTOSTART",
+    NO_AUTOSTART: "NO_AUTOSTART",
 
     // overwriteMode
-    OVERWRITE = "OVERWRITE",
-    NO_OVERWRITE = "NO_OVERWRITE",
+    OVERWRITE: "OVERWRITE",
+    NO_OVERWRITE: "NO_OVERWRITE",
 
     // debug out
-    ANI_DEBUG_PREFIX = "### Ani Debug -> ",
+    ANI_DEBUG_PREFIX: "### Ani Debug -> ",
 
     // easings mode
-    IN = 0,
-    IN_OUT = 2,
-    OUT = 1
+    IN: 0,
+    IN_OUT: 2,
+    OUT: 1
 };
